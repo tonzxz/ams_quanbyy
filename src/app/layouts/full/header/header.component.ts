f86e1c53298b759071155b20b6ce267d7ace951c
ryukkn <126628758+ryukkn@users.noreply.github.com>
2025-01-18 20:24:27 +0800

    header name

--- src/app/layouts/full/header/header.component.ts
+++ src/app/layouts/full/header/header.component.ts
@@ -11,16 +11,19 @@ import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { NgScrollbarModule } from 'ngx-scrollbar';
 import { MatButtonModule } from '@angular/material/button';
-import { UserService } from 'src/app/services/user.service';
+import { User, UserService } from 'src/app/services/user.service';
 import { BreadcrumbModule } from 'primeng/breadcrumb';
-import { MenuItem } from 'primeng/api';
+import { ConfirmationService, MenuItem } from 'primeng/api';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { DividerModule } from 'primeng/divider';
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [RouterModule, CommonModule, NgScrollbarModule, MaterialModule, MatButtonModule,
-    BreadcrumbModule
+    BreadcrumbModule,ConfirmDialogModule, DividerModule
   ],
   templateUrl: './header.component.html',
+  providers:[ConfirmationService],
   encapsulation: ViewEncapsulation.None,
 })
 
@@ -31,16 +34,64 @@ export class HeaderComponent implements OnInit {
   @Output() toggleCollapsed = new EventEmitter<void>();
 
   items:MenuItem[];
+  user?:User;
 
-  constructor(private userService:UserService) {}
+  intervalId: any;
+  currentTime: Date = new Date();
+  greeting: string;
+
+  constructor(private userService:UserService, private confirmationService:ConfirmationService) {}
+
+  getGreeting(): string {
+    const hour = this.currentTime.getHours();
+    if (hour < 12) {
+      return 'Good Morning 🌞';
+    } else if (hour < 18) {
+      return 'Good Afternoon 🌞';
+    } else {
+      return 'Good Evening 🌙';
+    }
+  }
 
   ngOnInit() {
+    this.user = this.userService.getUser();
+    this.greeting = this.getGreeting();
+    this.intervalId = setInterval(() => {
+      this.currentTime = new Date();
+      this.greeting = this.getGreeting();
+    }, 60000);  // 60000 milliseconds = 1 minute
     this.items = [
       { icon: 'pi pi-home', route: '/installation' }, 
       { label: 'Delivery Receipts' }, 
       { label: 'Supplier 01' }, 
       { label: 'DR-100391' }, 
     ];
+  }
+
+  confirmLogout(event: Event) {
+    this.confirmationService.confirm({
+        target: event.target as EventTarget,
+        message: 'Are you sure that you want to logout?',
+        header: 'Account Logout',
+        closable: true,
+        closeOnEscape: true,
+        icon: 'pi pi-info-circle',
+        rejectButtonProps: {
+            label: 'Cancel',
+            severity: 'secondary',
+            outlined: true,
+        },
+        acceptButtonProps: {
+            severity:'danger',
+            label: 'Logout',
+        },
+        accept: () => {
+            this.logout();
+        },
+        reject: () => {
+            
+        },
+    });
 }
 
   logout(){
