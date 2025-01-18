1ff75249b573e33d0c93e156c92b1f7825f0baed
ryukkn <126628758+ryukkn@users.noreply.github.com>
2025-01-18 19:23:53 +0800

    primeicons

--- src/app/layouts/full/header/header.component.ts
+++ src/app/layouts/full/header/header.component.ts
@@ -4,6 +4,7 @@ import {
   EventEmitter,
   Input,
   ViewEncapsulation,
+  OnInit,
 } from '@angular/core';
 import { MaterialModule } from 'src/app/material.module';
 import { RouterModule } from '@angular/router';
@@ -11,22 +12,40 @@ import { CommonModule } from '@angular/common';
 import { NgScrollbarModule } from 'ngx-scrollbar';
 import { MatButtonModule } from '@angular/material/button';
 import { UserService } from 'src/app/services/user.service';
-
+import { BreadcrumbModule } from 'primeng/breadcrumb';
+import { MenuItem } from 'primeng/api';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterModule, CommonModule, NgScrollbarModule, MaterialModule, MatButtonModule],
+  imports: [RouterModule, CommonModule, NgScrollbarModule, MaterialModule, MatButtonModule,
+    BreadcrumbModule
+  ],
   templateUrl: './header.component.html',
   encapsulation: ViewEncapsulation.None,
 })
 
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   @Input() showToggle = true;
   @Input() toggleChecked = false;
   @Output() toggleMobileNav = new EventEmitter<void>();
   @Output() toggleCollapsed = new EventEmitter<void>();
 
+  items:MenuItem[];
+  home:MenuItem;
+
   constructor(private userService:UserService) {}
+
+  ngOnInit(): void {
+    this.items = [
+      { label: 'Electronics' },
+      { label: 'Computer' },
+      { label: 'Accessories' },
+      { label: 'Keyboard' },
+      { label: 'Wireless' }
+    ];
+
+    this.home = { icon: 'pi pi-home', routerLink: '/' };
+  }
 
   logout(){
     this.userService.logout();
