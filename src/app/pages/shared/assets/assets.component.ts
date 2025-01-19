7fd443b252e1c1d4be4470a54da8357d427a0bf9
ryukkn <126628758+ryukkn@users.noreply.github.com>
2025-01-19 20:27:01 +0800

    assets langs

--- src/app/pages/shared/assets/assets.component.ts
+++ src/app/pages/shared/assets/assets.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, signal } from '@angular/core';
+import { MaterialModule } from 'src/app/material.module';
+import { DataViewModule } from 'primeng/dataview';
+import { StocksService } from 'src/app/services/stocks.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { SelectButtonModule } from 'primeng/selectbutton';
+import { ButtonModule } from 'primeng/button';
+import { TagModule } from 'primeng/tag';
 @Component({
   selector: 'app-assets',
   standalone: true,
-  imports: [],
+  imports: [MaterialModule,DataViewModule, CommonModule, FormsModule, SelectButtonModule, ButtonModule, TagModule],
   templateUrl: './assets.component.html',
   styleUrl: './assets.component.scss'
 })
-export class AssetsComponent {
+export class AssetsComponent implements OnInit {
+  assets = signal<any>([]);
+
+  options = ['list', 'grid'];
+
+  layout: 'list'|'grid' = 'grid';
+
+  constructor(private stockService:StocksService){}
+
+  ngOnInit(): void {
+    this.fetchItems();
+  }
+
+  async fetchItems(){
+    const stocks = await this.stockService.getAll();
+    this.assets.set([...stocks]);
+  }
 
 }
