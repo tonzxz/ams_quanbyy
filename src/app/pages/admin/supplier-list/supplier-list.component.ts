d4d5d8d3593a6950e6952b980c61e9c638b81d15
tonzxz <cabais1212@gmail.com>
2025-01-19 16:32:13 +0800

    sad aq

---
+++ src/app/pages/admin/supplier-list/supplier-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-supplier-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './supplier-list.component.html',
+  styleUrl: './supplier-list.component.scss'
+})
+export class SupplierListComponent {
+
+}
