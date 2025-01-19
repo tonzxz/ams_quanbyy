9aa4919cba89f2e13149ac3ea65d9f535fc8c65c
ryukkn <126628758+ryukkn@users.noreply.github.com>
2025-01-19 23:26:04 +0800

    receipt wiop

--- src/app/pages/shared/delivery-receipts/delivery-receipts.component.ts
+++ src/app/pages/shared/delivery-receipts/delivery-receipts.component.ts
@@ -8,24 +8,42 @@ import { ButtonModule } from 'primeng/button';
 import { ButtonGroupModule } from 'primeng/buttongroup';
 import { FluidModule } from 'primeng/fluid';
 import { TooltipModule } from 'primeng/tooltip';
-
+import { DialogModule } from 'primeng/dialog';
+import { InputTextModule } from 'primeng/inputtext';
+import { FileUploadModule, UploadEvent } from 'primeng/fileupload';
+import { DatePickerModule } from 'primeng/datepicker';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { MessageService } from 'primeng/api';
 @Component({
   selector: 'app-delivery-receipts',
   standalone: true,
-  imports: [MaterialModule,CommonModule, StepperModule, TableModule, ButtonModule, ButtonGroupModule, FluidModule, TooltipModule],
+  imports: [MaterialModule,CommonModule, StepperModule, TableModule, ButtonModule, ButtonGroupModule, 
+    FileUploadModule,DatePickerModule,InputNumberModule,
+    FluidModule, TooltipModule, DialogModule, InputTextModule],
+  providers:[MessageService],
   templateUrl: './delivery-receipts.component.html',
   styleUrl: './delivery-receipts.component.scss'
 })
 export class DeliveryReceiptsComponent implements OnInit {
   activeStep:number = 1;
 
+  uploadedFiles: File[] = [];
+
   receipts:DeliveryReceipt[]=[];
   filteredReceipts:DeliveryReceipt[]=[];
 
-  constructor(private deliveryService:DeliveryReceiptService){}
+  showReceiptModal:boolean = false;
+
+  constructor(
+    private messageService:MessageService,
+    private deliveryService:DeliveryReceiptService){}
 
   ngOnInit(): void {
     this.fetchItems();
+  }
+  
+  openReceiptModal(){
+    this.showReceiptModal = true;
   }
 
   filterByStatus(status:'unverified'|'processing'|'verified'){
@@ -34,6 +52,13 @@ export class DeliveryReceiptsComponent implements OnInit {
 
   getSeverityFromStatus(status:'unverified'|'processing'|'verified'){
     return status == 'verified' ? 'success' : 'secondary';
+  }
+
+  onUpload(event:any) {
+      for(let file of event.files) {
+          this.uploadedFiles.push(file);
+      }
+      this.messageService.add({severity: 'info', summary: 'File Uploaded', detail: ''});
   }
 
   async fetchItems(){
