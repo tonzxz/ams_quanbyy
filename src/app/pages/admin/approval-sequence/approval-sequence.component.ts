f6f92e15122d7c11d678aad3d09a9a06255fb5da
tonzxz <cabais1212@gmail.com>
2025-01-20 15:00:58 +0800

    approval

---
+++ src/app/pages/admin/approval-sequence/approval-sequence.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-approval-sequence',
+  standalone: true,
+  imports: [],
+  templateUrl: './approval-sequence.component.html',
+  styleUrl: './approval-sequence.component.scss'
+})
+export class ApprovalSequenceComponent {
+
+}
