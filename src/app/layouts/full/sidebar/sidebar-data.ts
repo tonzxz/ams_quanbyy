7799cdc5251eda545a7634eace15166ed6c8fa0d
SeanPalacay <palacaysean@gmail.com>
2025-01-23 20:57:22 +0800

    fix routes per role

--- src/app/layouts/full/sidebar/sidebar-data.ts
+++ src/app/layouts/full/sidebar/sidebar-data.ts
@@ -1,6 +1,7 @@
 import { NavItem } from './nav-item/nav-item';
 
 export const navItems: NavItem[] = [
+  // Common Items (No specific roles required)
   {
     navCap: 'Home',
   },
@@ -9,7 +10,7 @@ export const navItems: NavItem[] = [
     iconName: 'solar:widget-add-line-duotone',
     route: '/dashboard',
   },
-   {
+  {
     displayName: 'Approvals',
     iconName: 'solar:widget-add-line-duotone',
     route: '/shared/approvals',
@@ -19,242 +20,240 @@ export const navItems: NavItem[] = [
   {
     navCap: 'End User',
     divider: true,
-    requiredRoles: ['end-user'], // Only visible to end-user role
+    requiredRoles: ['end-user'],
   },
-  
   {
     displayName: 'Requisition',
     iconName: 'solar:documents-broken',
     route: '/enduser/requisition',
-    requiredRoles: ['end-user'], // Only visible to end-user role
+    requiredRoles: ['end-user'],
   },
   {
     displayName: 'Receiving',
     iconName: 'solar:documents-broken',
     route: '/enduser/receiving',
-    requiredRoles: ['end-user'], // Only visible to end-user role
+    requiredRoles: ['end-user'],
   },
 
   // Inspection Section
   {
     navCap: 'Inspection',
     divider: true,
-    requiredRoles: ['inspection', 'superadmin'], // Only visible to inspection and superadmin roles
+    requiredRoles: ['inspection', 'superadmin'],
   },
   {
     displayName: 'Dashboard',
     iconName: 'solar:widget-add-line-duotone',
     route: '/inspection/inspection-dashboard',
-    requiredRoles: ['inspection','superadmin']
+    requiredRoles: ['inspection', 'superadmin'],
   },
   {
     displayName: 'Purchase Requests',
     iconName: 'solar:documents-broken',
     route: '/inspection/purchase-request',
-    requiredRoles: ['inspection','superadmin']
+    requiredRoles: ['inspection', 'superadmin'],
   },
   {
     displayName: 'Receipt Approval',
     iconName: 'solar:documents-broken',
     route: '/inspection/receipt-approval',
-    requiredRoles: ['inspection', 'superadmin'], // Only visible to inspection and superadmin roles
+    requiredRoles: ['inspection', 'superadmin'],
   },
 
   // Supply Unit Section
   {
     navCap: 'Supply Unit',
     divider: true,
-    requiredRoles: ['supply', 'superadmin'], // Only visible to supply and superadmin roles
+    requiredRoles: ['supply', 'superadmin'],
   },
   {
     displayName: 'Delivery Receipts',
     iconName: 'solar:documents-broken',
     route: '/shared/delivery-receipts',
-    requiredRoles: ['supply', 'superadmin'], // Only visible to supply and superadmin roles
+    requiredRoles: ['supply', 'superadmin'],
   },
   {
     displayName: 'Supply Stocking',
     iconName: 'solar:inbox-archive-outline',
     route: '/supply-management/stocking',
-    requiredRoles: ['supply', 'superadmin'], // Only visible to supply and superadmin roles
+    requiredRoles: ['supply', 'superadmin'],
   },
   {
     displayName: 'Delivered Items',
     iconName: 'fluent:check-24-regular',
     route: '/shared/delivered-items',
-    requiredRoles: ['supply', 'superadmin'], // Only visible to supply and superadmin roles
+    requiredRoles: ['supply', 'superadmin'],
   },
   {
     displayName: 'Disbursement Vouchers',
     iconName: 'solar:ticket-sale-broken',
     route: '/shared/disbursement-vouchers',
-    requiredRoles: ['supply', 'superadmin'], // Only visible to supply and superadmin roles
+    requiredRoles: ['supply', 'superadmin'],
   },
 
-    {
+  // Accounting Section
+  {
     navCap: 'Accounting',
     divider: true,
-    requiredRoles: ['admin', 'superadmin'], // Only visible to admin and superadmin roles
+    requiredRoles: ['admin', 'superadmin', 'accounting'],
   },
   {
     displayName: 'Budget',
     iconName: 'solar:widget-add-line-duotone',
     route: '/accounting/budget',
-    requiredRoles: ['admin', 'superadmin'], // Only visible to admin and superadmin roles
+    requiredRoles: ['admin', 'superadmin'],
   },
   {
     displayName: 'Voucher Review',
     iconName: 'solar:ticket-sale-broken',
     route: '/accounting/voucher-review',
-    requiredRoles: ['admin', 'superadmin', 'accounting'], 
+    requiredRoles: ['admin', 'superadmin', 'accounting'],
   },
 
   // Admin Section
   {
     navCap: 'Admin',
     divider: true,
-    requiredRoles: ['admin', 'superadmin'], // Only visible to admin and superadmin roles
+    requiredRoles: ['admin', 'superadmin'],
   },
   {
     displayName: 'User Management',
     iconName: 'solar:widget-add-line-duotone',
     route: '/admin/user-management',
-    requiredRoles: ['admin', 'superadmin'], // Only visible to admin and superadmin roles
+    requiredRoles: ['admin', 'superadmin'],
   },
   {
     displayName: 'Department',
     iconName: 'solar:widget-add-line-duotone',
     route: '/admin/department',
-    requiredRoles: ['admin', 'superadmin'], // Only visible to admin and superadmin roles
+    requiredRoles: ['admin', 'superadmin'],
   },
   {
     displayName: 'Supplier List',
     iconName: 'solar:widget-add-line-duotone',
     route: '/admin/supplier-list',
-    requiredRoles: ['admin', 'superadmin'], // Only visible to admin and superadmin roles
+    requiredRoles: ['admin', 'superadmin'],
   },
-   {
+  {
     displayName: 'End-User List',
     iconName: 'solar:widget-add-line-duotone',
     route: '/admin/end-user-list',
-    requiredRoles: ['admin', 'superadmin'], // Only visible to admin and superadmin roles
+    requiredRoles: ['admin', 'superadmin'],
   },
   {
     displayName: 'Product Type',
     iconName: 'solar:widget-add-line-duotone',
     route: '/admin/product-type',
-    requiredRoles: ['admin', 'superadmin'], // Only visible to admin and superadmin roles
+    requiredRoles: ['admin', 'superadmin'],
   },
   {
     displayName: 'Approval Sequence',
     iconName: 'solar:widget-add-line-duotone',
     route: '/admin/approval-sequence',
-    requiredRoles: ['admin', 'superadmin'], // Only visible to admin and superadmin roles
+    requiredRoles: ['admin', 'superadmin'],
   },
   {
     displayName: 'Payment Terms',
     iconName: 'solar:widget-add-line-duotone',
     route: '/admin/payment-terms',
-    requiredRoles: ['admin', 'superadmin'], // Only visible to admin and superadmin roles
+    requiredRoles: ['admin', 'superadmin'],
   },
-
-
-    {
+  {
     displayName: 'Inventory',
     iconName: 'solar:widget-add-line-duotone',
     route: '/admin/inventory',
-    requiredRoles: ['admin', 'superadmin'], // Only visible to admin and superadmin roles
+    requiredRoles: ['admin', 'superadmin'],
   },
-
-   {
+  {
     displayName: 'Item Classification',
     iconName: 'solar:widget-add-line-duotone',
     route: '/admin/item-classification',
-    requiredRoles: ['admin', 'superadmin'], // Only visible to admin and superadmin roles
+    requiredRoles: ['admin', 'superadmin'],
   },
+
   // Views Section
   {
     navCap: 'Views',
     divider: true,
-    requiredRoles: ['supply', 'admin', 'superadmin'], // Only visible to supply, admin, and superadmin roles
+    requiredRoles: ['supply', 'admin', 'superadmin'],
   },
   {
     displayName: 'Inventory',
     iconName: 'solar:box-broken',
     route: '/shared/inventory',
-    requiredRoles: ['supply', 'superadmin'], // Only visible to supply and superadmin roles
+    requiredRoles: ['supply', 'superadmin'],
   },
 
-  // UI Components Section
-  {
-    navCap: 'Ui Components',
-    divider: true,
-  },
-  {
-    displayName: 'Badge',
-    iconName: 'solar:archive-minimalistic-line-duotone',
-    route: '/ui-components/badge',
-  },
-  {
-    displayName: 'Chips',
-    iconName: 'solar:danger-circle-line-duotone',
-    route: '/ui-components/chips',
-  },
-  {
-    displayName: 'Lists',
-    iconName: 'solar:bookmark-square-minimalistic-line-duotone',
-    route: '/ui-components/lists',
-  },
-  {
-    displayName: 'Menu',
-    iconName: 'solar:file-text-line-duotone',
-    route: '/ui-components/menu',
-  },
-  {
-    displayName: 'Tooltips',
-    iconName: 'solar:text-field-focus-line-duotone',
-    route: '/ui-components/tooltips',
-  },
-  {
-    displayName: 'Forms',
-    iconName: 'solar:file-text-line-duotone',
-    route: '/ui-components/forms',
-  },
-  {
-    displayName: 'Tables',
-    iconName: 'solar:tablet-line-duotone',
-    route: '/ui-components/tables',
-  },
+  // UI Components Section (No specific roles required)
+  // {
+  //   navCap: 'Ui Components',
+  //   divider: true,
+  // },
+  // {
+  //   displayName: 'Badge',
+  //   iconName: 'solar:archive-minimalistic-line-duotone',
+  //   route: '/ui-components/badge',
+  // },
+  // {
+  //   displayName: 'Chips',
+  //   iconName: 'solar:danger-circle-line-duotone',
+  //   route: '/ui-components/chips',
+  // },
+  // {
+  //   displayName: 'Lists',
+  //   iconName: 'solar:bookmark-square-minimalistic-line-duotone',
+  //   route: '/ui-components/lists',
+  // },
+  // {
+  //   displayName: 'Menu',
+  //   iconName: 'solar:file-text-line-duotone',
+  //   route: '/ui-components/menu',
+  // },
+  // {
+  //   displayName: 'Tooltips',
+  //   iconName: 'solar:text-field-focus-line-duotone',
+  //   route: '/ui-components/tooltips',
+  // },
+  // {
+  //   displayName: 'Forms',
+  //   iconName: 'solar:file-text-line-duotone',
+  //   route: '/ui-components/forms',
+  // },
+  // {
+  //   displayName: 'Tables',
+  //   iconName: 'solar:tablet-line-duotone',
+  //   route: '/ui-components/tables',
+  // },
 
-  // Auth Section
-  {
-    navCap: 'Auth',
-    divider: true,
-  },
-  {
-    displayName: 'Login',
-    iconName: 'solar:login-3-line-duotone',
-    route: '/authentication/login',
-  },
-  {
-    displayName: 'Register',
-    iconName: 'solar:user-plus-rounded-line-duotone',
-    route: '/authentication/register',
-  },
+  // Auth Section (No specific roles required)
+  // {
+  //   navCap: 'Auth',
+  //   divider: true,
+  // },
+  // {
+  //   displayName: 'Login',
+  //   iconName: 'solar:login-3-line-duotone',
+  //   route: '/authentication/login',
+  // },
+  // {
+  //   displayName: 'Register',
+  //   iconName: 'solar:user-plus-rounded-line-duotone',
+  //   route: '/authentication/register',
+  // },
 
-  // Extra Section
-  {
-    navCap: 'Extra',
-    divider: true,
-  },
-  {
-    displayName: 'Icons',
-    iconName: 'solar:sticker-smile-circle-2-line-duotone',
-    route: '/extra/icons',
-  },
-  {
-    displayName: 'Sample Page',
-    iconName: 'solar:planet-3-line-duotone',
-    route: '/extra/sample-page',
-  },
+  // Extra Section (No specific roles required)
+  // {
+  //   navCap: 'Extra',
+  //   divider: true,
+  // },
+  // {
+  //   displayName: 'Icons',
+  //   iconName: 'solar:sticker-smile-circle-2-line-duotone',
+  //   route: '/extra/icons',
+  // },
+  // {
+  //   displayName: 'Sample Page',
+  //   iconName: 'solar:planet-3-line-duotone',
+  //   route: '/extra/sample-page',
+  // },
 ];
